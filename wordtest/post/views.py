0338652d54e31da9bac902cc4d600bd31682fb31
Jaehong Kim <jaehong1007@gmail.com>
2018-09-27 15:39:58 +0900

    단어장 시작

---
+++ wordtest/post/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+import pandas as pd
+import random
+import csv
+from django.views.decorators.csrf import csrf_protect
+
+
+class AnswerSheet:
+    word_list = []
+    wrong_list = []
+
+    def get_words(self, word):
+        self.word_list.append(word)
+
+    def save_wrong_words(self, word):
+        self.wrong_list.append(word)
+
+
+# Create your views here.
+@csrf_protect
+def post_list(request):
+    df = pd.read_csv('words.csv', encoding='cp949')
+    words = df['word']
+
+    # 단어 & 뜻 선택
+    word = random.choice(words)
+
+    # 중복되면 넘어가도록 설정해야함
+    answers = AnswerSheet()
+    if len(answers.word_list) == len(words):
+        return render(request, 'blog/finish.html')
+    if word in answers.word_list:
+        while word in answers.word_list:
+            word = random.choice(words)
+            if word not in answers.word_list:
+                break
+
+    answers.get_words(word)
+
+    meaning = df[words == word]['meaning'].values[0]
+
+    # 다른 보기 만들기
+    choices = random.sample(set(df[df['meaning'] != meaning]['meaning']), 3)
+    choices.append(meaning)
+    random.shuffle(choices)
+    return render(request, 'blog/post_list')
+
